a649592363d04b54b23ccdbe774ba0f3da211f4e
NickBueltel <nickbueltel@gmail.com>
2019-04-02 21:37:32 -0500

    started a journal, did some other small things.

---
+++ mathcalc.js
@@ -0,0 +1,22 @@
+// going to make some simple math calculations for "fun"
+// 4.2.19
+//nick bueltel
+
+function add(num1, num2){
+    sum = ( num1 + num2);
+    return sum;
+}
+
+console.log(add(1, 4));
+
+
+function subtract(num1, num2){
+    subsum = (num1 - num2);
+    return subsum;
+}
+
+console.log(subtract(4,1));
+
+
+
+
