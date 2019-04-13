5087285d9e8e05bc04612cb910edab739396c457
Nick-Bueltel <nickbueltel@gmail.com>
2019-04-13 18:55:49 -0500

    updated stuff

---
+++ venv/catclicker.js
@@ -0,0 +1,10 @@
+
+
+
+
+var elem = document.getElementById('my-elem');
+elem.addEventListener('click', function(){
+    //the element has been clicked... do stuff here
+    var countingCats = 0;
+    countingCats = countingCats + 1;
+}, false);
