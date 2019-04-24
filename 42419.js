476402db47e051cc4d29db3428e921125b592a84
Nick-Bueltel <nickbueltel@gmail.com>
2019-04-24 11:03:33 -0500

    Create 42419.js

---
+++ 42419.js
@@ -0,0 +1,5 @@
+//4.24.19 
+// arrays stuff 
+
+const classMates = ['Gideon', 'Jeremy', 'Omar', 'Codee', 'Nick', 'Rain', 'BUT Sam is fine','Ricardo', 'Crysta', 'Brandon', 'Jenny', 'Joseph', 'Hayden', 'Aaron', 'Dana', 'Chris', 'Andrew' ];
+    console.log(classMates);
